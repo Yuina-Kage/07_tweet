f4b00e6b9d71bb8142096174b4012650fcdb12ab
Yuina-Kage <yuina.kg@gmail.com>
2020-04-08 14:46:25 +0900

    アドバイスお願い致します

--- edit.php
+++ edit.php
@@ -1,5 +1,61 @@
 <?php
+
+require_once('config.php');
+require_once('functions.php');
+
+$id = $_GET['id'];
+
+$errors = array();
+
+$dbh = connectDb();
+$sql = 'SELECT * FROM tweets WHERE id = :id';
+$stmt = $dbh->prepare($sql);
+$stmt->bindParam(':id', $id, PDO::PARAM_INT);
+$stmt->execute();
+
+$tweet = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if (!$tweet) {
+  header('Location: index.php');
+  exit;
+  }
+
+
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  
+  $content = $_POST['content'];
+  $errors = [];
+  
+  
+  if ($content ==$tweet['content']) {
+    $errors[] = '内容が変更されていません';
+  }
+  
+  if ($title == '') {
+    $errors[] = '入力がされていません';
+  }
+
+
+  if (empty($errors)) {
+    $dbh = connectDb();
+    $sql = 'UPDATE tweets SET content = :content, good = :good, cteated_at = now()
+            WHERE id = :id';
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->bindParam(':content', $content, PDO::PARAM_STR);
+    $stmt->bindParam(':good', $good, PDO::PARAM_STR);
+    $stmt->execute();
+    
+    header('Location: index.php');
+    exit;
+  }
+}
+
 ?>
+
+
 
 
 
@@ -7,19 +63,31 @@
 
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>tweetの編集</title>
 </head>
+
 <body>
   <h1>tweetの編集</h1>
-  <a href="index.html">戻る</a><br>
+  <a href="index.php">戻る</a><br>
+  <?php if ($errors) : ?>
+    <ul class="error-list">
+      <?php foreach ($errors as $error) : ?>
+        <li>
+          <?php echo h($error); ?>
+        </li>
+      <?php endforeach; ?>
+    </ul>
+  <?php endif; ?>
   <br>
   <label>ツイート内容<br>
-  <textarea name="テキストエリア" placeholder="テストツイート" rows="5" cols=32></textarea><br>
-  <br> 
+    <textarea name="テキストエリア" placeholder="テストツイート" rows="5" cols=32><?php echo h($tweet['content']); ?></textarea><br>
+    <br>
     <input type="submit" value="編集する">
   </label>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 
 require_once('config.php');
+require_once('functions.php');
 
 $dbh = connectDb();
 
-$sql = "select * from tweets order by updated_at desc";
+$sql = 'SELECT * FROM tweets ORDER BY created_ad DESC';
 $stmt = $dbh->prepare($sql);
 $stmt->execute();
 $tweets = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -12,12 +13,21 @@ $tweets = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  
   $content = $_POST['content'];
-
   $errors = [];
 
   if ($content == '') {
     $errors[] = 'ツイート内容を入力してください';
+  }
+
+  if (empty($errors)) {
+    $dbh = connectDb();
+    $sql = 'INSERT INTO tweets (content, created_at) VALUES (:content, now())';
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindParam(':content', $content, PDO::PARAM_STR);
+    $stmt->execute();
+  
   }
 }
 
@@ -30,29 +40,48 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Tweet一覧</title>
   <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
   <h1>新規Tweet</h1>
   <?php if ($errors) : ?>
-      <ul class="error-list">
-        <?php foreach ($errors as $error) : ?>
-          <li><?php echo $error; ?></li>
-        <?php endforeach; ?>
-      </ul>
+    <ul class="error-list">
+      <?php foreach ($errors as $error) : ?>
+        <li><?php echo $error; ?></li>
+      <?php endforeach; ?>
+    </ul>
   <?php endif; ?>
-  
+
   <form action="" method="post">
-  <label for="content">ツイート内容<br>
-    <textarea name="テキストエリア" placeholder="いまどうしてる？" rows="5" cols=32></textarea><br>
-  <br> 
-    <input type="submit" value="投稿する">
-  </label>
+    <label for="content">ツイート内容<br>
+      <textarea name="テキストエリア" placeholder="いまどうしてる？" rows="5" cols=32></textarea><br>
+      <br>
+      <input type="submit" value="投稿する">
+    </label>
   </form>
   <h2>Tweet一覧</h2>
+  <?php if (count($tweets)) : ?>
+      <ul class="tweet-list">
+        <?php foreach ($tweets as $tweet) : ?>
+          <li>
+            
+            <a href="show.php?id=<?php echo h($tweet['id']) ?>">
+            <?php echo h($tweet['content']); ?></a><br>
+            <?php echo h($tweet['good']); ?><br>
+            投稿日時: <?php echo h($tweet['created_at']); ?>
+            <hr>
+          </li>
+        <?php endforeach; ?>
+      </ul> 
+    <?php  else : ?>  
+      <p>投稿されたtweetはありません</p>
+    <?php endif; ?>
+      
 </body>
 </html>
